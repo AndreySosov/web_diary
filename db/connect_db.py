f8693573007af9b003d2c4e2187802feebe37566
Vladimir <v.gorobets77@mail.ru>
2022-11-08 16:50:15 +0300

    создание БД. подключение к БД.

---
+++ db/connect_db.py
@@ -0,0 +1,37 @@
+import psycopg2
+from psycopg2 import Error
+
+import config
+
+
+def connect_db():
+    try:
+        # Подключение к существующей базе данных
+        connection = psycopg2.connect(user="postgres",
+                                      # пароль, который указали при установке PostgreSQL
+                                      password=config.PASS_DB,
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="web_diary_db")
+
+        # Курсор для выполнения операций с базой данных
+        cursor = connection.cursor()
+        # Распечатать сведения о PostgreSQL
+        print("Информация о сервере PostgreSQL")
+        print(connection.get_dsn_parameters(), "\n")
+        # Выполнение SQL-запроса
+        cursor.execute("SELECT version();")
+        # Получить результат
+        record = cursor.fetchone()
+        print("Вы подключены к - ", record, "\n")
+
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Соединение с PostgreSQL закрыто")
+
+
+connect_db()
